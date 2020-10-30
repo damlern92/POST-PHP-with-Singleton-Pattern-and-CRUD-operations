bc5a7630e5229bff68d667373b45da3d91e977c3
Damjanac Marko <36675797+damlern92@users.noreply.github.com>
2020-10-30 20:30:01 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -8,11 +8,8 @@ DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_USER', 'root');
 DEFINE ('DB_PASS', '');
 DEFINE ('DB_NAME', 'singleton_crud');
-DEFINE ('APP_DIR', 'c:/wamp64/www/virtual.a/singleton');
 
-spl_autoload_register('myAutoloader');
 
-function myAutoloader($className)
-{
-    require_once APP_DIR . "/classes/" . $className . ".php";
-}
+spl_autoload_register(function($className){
+    require($_SERVER['DOCUMENT_ROOT'] ."/singleton-crud/_app/classes/{$className}.php");
+});
--- insert_post.php
+++ insert_post.php
@@ -1,8 +1,13 @@
-<?php include_once "includes/header.php"; ?>
+<?php 
+
+include_once "includes/header.php";
+
+ ?>
 
 <h2>Insert new Post</h2>
 
 <?php
+	// PART 1:
 	if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])){
 		// pass parameters to insertNewPost method:
 		$insertPost = $post->insertNewPost($_POST);
@@ -12,39 +17,43 @@
 		echo $insertPost;
 	}
 ?>
+	
 
+	 <!-- PART 2: -->
 <form action="#" method="post" enctype="multipart/form-data">
-		<br>
-	    <label for="frmName">Title:</label>
-	    <input type="text" id="frmName" name="title">
-	    <br><br>
+	<br>
+    <label for="frmName">Title:</label>
+    <input type="text" id="frmName" name="title">
+    <br><br>
 
-		<label for="sell">Select Category:</label>
-		<select name="cat_id" id="sel1">
-			<option value="null">Select Category</option>
-			<?php
-			// The Categories from mySQL database:
-				$getCat = $post->getAllCategories();
-				while($result = $getCat->fetch_object()) {
-					$cat_id = $result->category_id;
-					$cat_title = $result->category_title;
-					echo "<option value='$cat_id'>$cat_title</option>";
-				}
-			?>
+    <!-- Select: -->
+	<label for="sell">Select Category:</label>
+	<select name="cat_id" id="sel1">
+		<option value="null">Select Category</option>
+		<?php
+		// The Categories from mySQL database:
+			$getCat = $post->getAllCategories();
+			while($result = $getCat->fetch_object()) {
+				$cat_id = $result->category_id;
+				$cat_title = $result->category_title;
+				echo "<option value='$cat_id'>$cat_title</option>";
+			}
+		?>
+	</select>
 
-		</select>
-		<br><br>
+	<br><br>
 
-		<label for="image">Image:</label>
-		<input type="file" name="post_image" id="image">
-		<br><br>
+	<!-- Input field 4: -->
+	<label for="image">Image:</label>
+	<input type="file" name="post_image" id="image">
+	<br><br>
 
-		<label for="frmText">Your message:</label><br>
-	    <textarea name="content" id="frmText" rows="3" cols="30"></textarea>
-	    <br><br>
+	<label for="frmText">Your message:</label><br>
+    <textarea name="content" id="frmText" rows="3" cols="30"></textarea>
+    <br><br>
 
-	    <input type="submit" name="submit" value="Insert" id="frmSubmit">
 
-	    <input type="reset" name="frmReset" value="Reset" id="frmReset">
+    <input type="submit" name="submit" value="Insert" id="frmSubmit">
+    <input type="reset" name="frmReset" value="Reset" id="frmReset">
 
 </form>
